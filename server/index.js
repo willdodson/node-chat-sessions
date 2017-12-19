d7db97aacc4efd20e5c2aabd3d10f203de5e7e2c
wsdodson <wsdodson@gmail.com>
2017-12-19 12:34:40 -0700

    step2

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+require('dotenv').config();
+const session = require('express-session');
+
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 10000 }
+}
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
